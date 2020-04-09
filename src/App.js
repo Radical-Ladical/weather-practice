e65cbddbf1f389fb33cf9aeeb324aceba96fd13c
Student_M14 <student_m14@PS-DAL-M14.home>
2020-04-09 11:42:02 -0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import Weather from './components/weather';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      weather: null,
+    }
+  }
+
+  componentDidMount() {
+    this.getWeatherData();
+  }
+
+  //------async way---\\
+  // async getWeatherData() {
+  //   try {
+
+  //     const response = await axios.get("https://api.openweathermap.org/data/2.5/forecast?q=london&appid=e4447104f032368a2a14af1ccf6fd22e&units=imperial");
+  //     console.log(response.data);
+
+  //     this.setState({ weather: response.data });
+
+  //   }
+  //   catch(error) {
+  //     console.log(error)
+  //   }
+  // }
+  //------------\\\
+
+
+  //---using get and fetch---\\
+  getWeatherData() {
+    axios.get('https://api.openweathermap.org/data/2.5/forecast?q=london&appid=e4447104f032368a2a14af1ccf6fd22e&units=imperial')
+    .then(response => {
+      // console.log(response.data);
+      this.setState({ weather: response.data });
+    }).catch(error => console.error(error));
+  }
+
+  nullCheck() {
+    if(this.state.weather===null){
+      <h2>no information</h2>
+    }else {
+      this.state.weather.list.map(listItem => <Weather/>);
+    }
+  }
+ 
+  render() {
+    return(
+      <div>
+        {
+          this.nullcheck()
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
